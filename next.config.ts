f6c8b3bd36c471351ccd376bb9281576ff4d1259
inetimimizzle <enaibreemma@gmail.com>
2026-01-30 13:09:11 +0100

    feat: add R2 CDN hostname to next.config.ts for Image component

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'pub-7fffc38932334c9cb56dced971206600.r2.dev',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
